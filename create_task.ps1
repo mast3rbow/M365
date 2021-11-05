3fdcbd4b94cfd2ae553a76bdbd56051953aa13af
Bryton Wishart <bryton@curedcompliance.com>
2021-11-05 10:43:38 +1000

    initial upload

---
+++ create_task.ps1
@@ -0,0 +1,40 @@
+﻿<#
+===================================================
+.NOTES
+ Organization: Telstra
+ Author: Bryton I. Wishart
+ Filename: NetworkAssessment.ps1
+ ===================================================
+
+<#
+THis script will automatically create the 
+#>
+
+# Customer Variables
+$customer = ""
+$site = ""
+$date = (Get-Date).Date
+
+
+# Scheduled Task Variables
+$name = "TeamsNetworkAssessment"
+ 
+
+# Webhook Reciever Information
+$webhook = ""
+# Install the Network Assessment Tool locally on the PC
+
+
+# Powershell script name
+
+
+# Create schedulded Job
+Register-ScheduledJob -Name $name -Trigger $trigger -ScriptBlock $action -MaxResultCount 4
+Start-Sleep -Seconds 5
+Get-ScheduledJob -name $name
+
+$task = Get-ScheduledTask -TaskName $Name
+$task.Triggers.repetition.Duration = 'PT30M'
+$task | Set-ScheduledTask
+
+
---
+++ networkassessment.ps1
@@ -0,0 +1,46 @@
+﻿<#
+===================================================
+.NOTES
+ Organization: Perficient Inc.
+ Filename: NetworkAssessment.ps1
+ ===================================================
+PowerShell script to automate the Microsoft Network Assessment Tool
+Step 1 - Create NetworkAssessment folder at root of C:
+Step 2 - Copy network assessment PowerShell script to install directory of the network assessment tool
+Step 3 - Create scheduled task to run the PowerShell(AS SHOWN BELOW)
+Step 4 - Modify network assessment config file parameters
+powershell -nologo -noninteractive -command & ("{C:\Program Files\Microsoft Skype for Business Network Assessment Tool\NetworkAssessment.ps1}")
+#>
+<#
+Start the Network Assessment Process
+Wait for process to complete before renaming the file in order to prevent overwriting prior data
+#>
+push-location "C:\Program Files\Microsoft Skype for Business Network Assessment Tool";
+$exe = "NetworkAssessmentTool" 
+$proc = (Start-Process $exe -PassThru)
+$proc | Wait-Process
+<#
+Rename output file
+#>
+$file = Get-Item C:\Networkassessment\performance_results.*
+
+$TSVjson = Import-Csv -Delimiter "`t" -Path $file | ConvertTo-Json -Compress | Add-content -Path "output.json"
+
+
+# JSON object - creates the Logging object to be sent to the webhook reciever
+$obj = New-object -Type psobject
+$obj | Add-Member -MemberType NoteProperty -Name Customer -Value $customer -Force
+$obj | Add-Member -MemberType NoteProperty -Name Site -Value $site -Force
+$obj | Add-Member -MemberType NoteProperty -Name datetime -Value $date -Force
+
+
+# Constructor script to put object into array
+$final_data="["
+$final_data+=$obj | ConvertTo-Json
+$final_data=+","+$TSVjson+"]"
+
+log $final_data
+
+
+# sends the restful response t owebhook
+#Invoke-RestMethod -Uri $webhook -Method POST -Body $final_data -ContentType "application/json"
