3142a810e7cc969937f3b84b9c73bc7aa6ee1493
Bryton Wishart <bryton.wishart@entag.com.au>
2022-01-19 11:54:12 +1000

    Create exchange.ps1

---
+++ blueprints/exchange.ps1
@@ -0,0 +1,87 @@
+        EXOAtpPolicyForO365
+        {
+            AllowClickThrough             = $False;
+            AllowSafeDocsOpen             = $False;
+            BlockUrls                     = @();
+            Credential                    = $Credscredential;
+            EnableATPForSPOTeamsODB       = $False;
+            EnableSafeDocs                = $False;
+            EnableSafeLinksForO365Clients = $True;
+            Ensure                        = "Present";
+            IsSingleInstance              = "Yes";
+            TrackClicks                   = $False;
+        }
+        EXOHostedContentFilterPolicy
+        {
+            AddXHeaderValue                      = "";
+            AdminDisplayName                     = "";
+            BulkSpamAction                       = "MoveToJmf";
+            BulkThreshold                        = 6;
+            Credential                           = $Credscredential;
+            DownloadLink                         = $False;
+            EnableEndUserSpamNotifications       = $True;
+            EnableLanguageBlockList              = $False;
+            EnableRegionBlockList                = $False;
+            EndUserSpamNotificationCustomSubject = "";
+            EndUserSpamNotificationFrequency     = 3;
+            EndUserSpamNotificationLanguage      = "Default";
+            Ensure                               = "Present";
+            HighConfidencePhishAction            = "Quarantine";
+            HighConfidenceSpamAction             = "Quarantine";
+            Identity                             = "Standard Preset Security Policy";
+            IncreaseScoreWithBizOrInfoUrls       = "Off";
+            IncreaseScoreWithImageLinks          = "Off";
+            IncreaseScoreWithNumericIps          = "Off";
+            IncreaseScoreWithRedirectToOtherPort = "Off";
+            InlineSafetyTipsEnabled              = $True;
+            LanguageBlockList                    = @();
+            MakeDefault                          = $False;
+            MarkAsSpamBulkMail                   = "On";
+            MarkAsSpamEmbedTagsInHtml            = "Off";
+            MarkAsSpamEmptyMessages              = "Off";
+            MarkAsSpamFormTagsInHtml             = "Off";
+            MarkAsSpamFramesInHtml               = "Off";
+            MarkAsSpamFromAddressAuthFail        = "Off";
+            MarkAsSpamJavaScriptInHtml           = "Off";
+            MarkAsSpamNdrBackscatter             = "Off";
+            MarkAsSpamObjectTagsInHtml           = "Off";
+            MarkAsSpamSensitiveWordList          = "Off";
+            MarkAsSpamSpfRecordHardFail          = "Off";
+            MarkAsSpamWebBugsInHtml              = "Off";
+            ModifySubjectValue                   = "";
+            PhishSpamAction                      = "Quarantine";
+            PhishZapEnabled                      = $True;
+            QuarantineRetentionPeriod            = 30;
+            RedirectToRecipients                 = @();
+            RegionBlockList                      = @();
+            SpamAction                           = "MoveToJmf";
+            SpamZapEnabled                       = $True;
+            TestModeAction                       = "None";
+            TestModeBccToRecipients              = @();
+        }
+        EXOMailTips
+        {
+            Credential                            = $Credscredential;
+            Ensure                                = "Present";
+            MailTipsAllTipsEnabled                = $True;
+            MailTipsExternalRecipientsTipsEnabled = $False;
+            MailTipsGroupMetricsEnabled           = $True;
+            MailTipsLargeAudienceThreshold        = 25;
+            MailTipsMailboxSourcedTipsEnabled     = $True;;
+        }
+        EXOMalwareFilterPolicy
+        {
+            Action                                 = "DeleteMessage";
+            Credential                             = $Credscredential;
+            CustomNotifications                    = $False;
+            EnableExternalSenderAdminNotifications = $False;
+            EnableExternalSenderNotifications      = $False;
+            EnableFileFilter                       = $True;
+            EnableInternalSenderAdminNotifications = $False;
+            EnableInternalSenderNotifications      = $False;
+            Ensure                                 = "Present";
+            FileTypes                              = @("ace","ani","app","cab","docm","exe","iso","jar","jnlp","reg","scr","vbe","vbs");
+            Identity                               = "Standard Preset Security Policy;
+            MakeDefault                            = $False;
+            ZapEnabled                             = $True;
+        }
